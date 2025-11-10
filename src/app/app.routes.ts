e49264b7074a9f45b018fa01216b499f7f9bc045
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-10 17:16:34 +0100

    added import and route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './components/books/books';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/books', pathMatch: 'full' },
+    { path: 'books', component: BooksComponent}
+];
