e66140559a135d36d64617bc95d73a422425e609
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-10 16:32:10 +0100

    added code to send httprequests

--- src/app/services/book.ts
+++ src/app/services/book.ts
@@ -7,5 +7,21 @@ import { Observable } from 'rxjs';
 export class BookService {
   private apiUrl = 'http://localhost:5176/api/books';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
+
+  getBooks(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addBook(book: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, book);
+  }
+
+  updateBook(id: number, book: any): Observable<any> {
+    return this.http.put<any>(`${this.apiUrl}/${id}`, book);
+  }
+
+  deleteBook(id: number, book: any): Observable<any> {
+    return this.http.delete<any>(`${this.apiUrl}/${id}`);
+  }
 }
