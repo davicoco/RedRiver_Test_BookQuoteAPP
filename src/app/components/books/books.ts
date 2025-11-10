2b2b071e7231021eefc10fe0789a8f8d2890f21d
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-10 17:01:42 +0100

    added code to manage and display books

--- src/app/components/books/books.ts
+++ src/app/components/books/books.ts
@@ -4,10 +4,31 @@ import { BookService } from '../../services/book';
 
 @Component({
   selector: 'app-books',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './books.html',
   styleUrl: './books.css',
 })
-export class Books {
+export class BooksComponent {
+  books: any[] = [];
+  isloading = false;
 
+  constructor(private bookService: BookService) { }
+
+  ngOnInit() {
+    this.loadBooks();
+  }
+
+  loadBooks() {
+    this.isloading = true;
+    this.bookService.getBooks().subscribe({
+      next: (data) => {
+        this.books = data;
+        this.isloading = false;
+      },
+      error: (error) => {
+        console.error('Error loading books:', error);
+        this.isloading=false;
+      }
+    });
+  }
 }
