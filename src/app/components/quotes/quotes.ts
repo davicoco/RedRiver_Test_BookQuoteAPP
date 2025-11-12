34ac26929f69cd272d3fe168384468574f1cba95
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-12 09:54:03 +0100

    added imports

--- src/app/components/quotes/quotes.ts
+++ src/app/components/quotes/quotes.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { QuoteService } from '../../services/quote';
 
 @Component({
   selector: 'app-quotes',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './quotes.html',
   styleUrl: './quotes.css',
 })
