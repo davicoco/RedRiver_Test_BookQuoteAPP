dfdfa2d86bc0e60ba81a1541dfc109afbea093d8
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-13 19:26:32 +0100

    implemented authGuard and protected endpoints books and quotes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,22 @@ import { BooksComponent } from './components/books/books';
 import { QuotesComponent } from './components/quotes/quotes';
 import { LoginComponent } from './components/login/login';
 import { RegisterComponent} from './components/register/register';
+import { authGuard } from './guards/auth-guard'
 
 export const routes: Routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent},
     { path: 'register', component: RegisterComponent},
     { path: 'books', component: BooksComponent },
-    { path: 'quotes', component: QuotesComponent}
+    { path: 'quotes', component: QuotesComponent},
+    {
+        path:'books',
+        component: BooksComponent,
+        canActivate: [authGuard]
+    },
+    {
+        path:'quotes',
+        component: QuotesComponent,
+        canActivate: [authGuard]
+    }
 ];
