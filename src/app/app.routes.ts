065b37be30ae2bdd4107b785dcdd7db871b4d6ac
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-13 17:32:11 +0100

    added route and html for loginpage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { BooksComponent } from './components/books/books';
 import { QuotesComponent } from './components/quotes/quotes';
+import { LoginComponent } from './components/login/login';
 
 export const routes: Routes = [
-    { path: '', redirectTo: '/books', pathMatch: 'full' },
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent},
     { path: 'books', component: BooksComponent },
     { path: 'quotes', component: QuotesComponent}
 ];
