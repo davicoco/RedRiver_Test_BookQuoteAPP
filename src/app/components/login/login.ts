6de7dbd1a64f052baa2bb065313063bc0a13ea38
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-13 16:50:48 +0100

    added login component with form and responses for invalid credentials

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,11 +1,61 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
 
+export class LoginComponent {
+  loginForm: FormGroup;
+  errorMessage: string = '';
+  isLoading: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router
+  ) {
+    //login formulär
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    this.authService.login(this.loginForm.value).subscribe({
+      next: (response) => {
+        console.error('Lyckad inloggning', response);
+        this.router.navigate(['/books']);
+      },
+      error: (error) => {
+        console.error('Login misslyckad', error);
+        this.errorMessage = error.error?.message || 'Inloggning misslyckad kontrollera dina uppgifter'
+        this.isLoading = false;
+      },
+      complete: () => {
+        this.isLoading = false;
+      }
+    });
+  }
+
+  //checkar om fält är ogiltigt eller 'rört'
+  isFieldInvalid(fieldName: string): boolean {
+    const field = this.loginForm.get(fieldName);
+    return !!(field && field.invalid && (field.dirty || field.touched));
+  }
 }
