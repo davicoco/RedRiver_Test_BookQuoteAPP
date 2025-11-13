47f75d220a066edfba700ef4f4955b043672eb7a
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-13 23:53:19 +0100

    injected navbar with AuthService and updated navbar with clickables for login and register page

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,12 +1,20 @@
-import { Component  } from '@angular/core';
-import { RouterLink, RouterLinkActive} from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../../services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class NavbarComponent {
+
+  constructor(public authService: AuthService) { }
+
+  logout(): void {
+    this.authService.logout();
+  }
 
 }
