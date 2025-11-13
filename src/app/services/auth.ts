4075d12f7ee0825db10d14ba4d7c9097675eee47
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-13 16:15:32 +0100

    implemented methods for login logout register with DTos

--- src/app/services/auth.ts
+++ src/app/services/auth.ts
@@ -1,8 +1,75 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, BehaviorSubject, tap } from 'rxjs';
+import { Router } from '@angular/router';
+
+interface RegisterDto {
+  username: string;
+  email: string;
+  password: string;
+}
+
+interface LoginDto {
+  email: string;
+  password: string;
+}
+
+interface AuthResponse {
+  token: string;
+  email: string;
+}
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class Auth {
-  
+
+export class AuthService {
+  private apiUrl = 'http://localhost:5176/auth';
+
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(this.hasToken());
+  public isAuthenticated$ = this.isAuthenticatedSubject.asObservable();
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  register(registerDto: RegisterDto): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/register`, registerDto)
+      .pipe(
+        tap(response => {
+          this.saveToken(response.token);
+          this.isAuthenticatedSubject.next(true);
+        })
+      );
+  }
+
+  login(loginDto: LoginDto): Observable<AuthResponse> {
+    return this.http.post<AuthResponse>(`${this.apiUrl}/login`, loginDto)
+      .pipe(
+        tap(response => {
+          this.saveToken(response.token);
+          this.isAuthenticatedSubject.next(true);
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.isAuthenticatedSubject.next(false)
+    this.router.navigate(['/login'])
+  }
+
+  private saveToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token')
+  }
+
+  private hasToken(): boolean {
+    return !!this.getToken();
+  }
+
+  isLoggedIn(): boolean {
+    return this.hasToken();
+  }
 }
