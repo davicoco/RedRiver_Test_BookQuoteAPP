b792185c9f2915a609ab400889ebf0d9de70bc64
David Izquierdo Puentes <david_izqpue@hotmail.com>
2025-11-14 09:08:02 +0100

    implemented interceptor and added to config

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,5 +1,19 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn} from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../services/auth';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
-};
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  if(token){
+    const clonedRequest = req.clone({
+      setHeaders: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+    return next(clonedRequest);
+  }
+
+  return next(req)
+}
